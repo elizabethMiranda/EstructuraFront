48e0421df52e28198e7371ecf5b9593f6601bd43
emirandac <elizabeth.miranda@dgtid.oaxaca.gob.mx>
2024-09-11 21:54:42 -0600

    separando componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { PagesRoutingModule } from './pages/pages-routing';
 
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { AuthRoutingModule } from './auth/auth-routing';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
