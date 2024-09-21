68d941a1444e76de58868d0e3aa398275da2dd9d
emirandac <elizabeth.miranda@dgtid.oaxaca.gob.mx>
2024-09-21 12:50:27 -0600

    agrega funcionalidad de grafica

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from '../components/components.module';
 
+import { NgChartsModule } from 'ng2-charts';
+
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -10,6 +12,7 @@ import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 
 
@@ -18,14 +21,15 @@ import { FormsModule } from '@angular/forms';
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent],
+    PagesComponent,
+    AccountSettingsComponent],
   imports: [
     FormsModule,
     CommonModule,
     SharedModule,
     RouterModule,
-    ComponentsModule
-  ],
+    ComponentsModule,
+    NgChartsModule],
   exports:[
     DashboardComponent,
     ProgressComponent,
